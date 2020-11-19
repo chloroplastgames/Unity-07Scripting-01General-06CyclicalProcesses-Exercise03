acee6ede5047a34c70fce38427b7ffc728bcc5c3
ValdeirLevelUP <72570501+ValdeirLevelUP@users.noreply.github.com>
2020-11-19 17:06:39 +0100

    almost complete

---
+++ Assets/Scripts/Data/DataPlayer.cs
@@ -0,0 +1,27 @@
+﻿/// INFORMATION
+///
+/// Project:
+/// Game:
+/// Data:
+/// Author:
+/// Programmers:
+/// Description:
+///
+using UnityEngine;
+[CreateAssetMenu(menuName = "Prototype/Data player")]
+public class DataPlayer:ScriptableObject
+{
+    #region PRIVATE VARIABLE
+
+    [SerializeField] private float _maxLife;
+
+    [SerializeField] private float _currentLife;
+
+    #endregion
+
+    #region PROPERTIES
+
+    public float VidaMaxima { get => _maxLife; }
+    public float CurrentLife { get => _currentLife; set => _currentLife = value; }
+    #endregion
+}
---
+++ Assets/Scripts/Exceptions/NegativeValueException.cs
@@ -0,0 +1,33 @@
+﻿/// INFORMATION
+///
+/// Project:
+/// Game:
+/// Data:
+/// Author:
+/// Programmers:
+/// Description:
+///
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class NegativeValueException : UnityException
+{
+    public NegativeValueException()
+    {
+    }
+
+    public NegativeValueException(string message) : base(message)
+    {
+    }
+
+    public NegativeValueException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected NegativeValueException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
